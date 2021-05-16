b0b180c965ac03d10cbd00e2069432bdd032e4c9
nagisa <2861963320@qq.com>
2021-05-16 22:58:47 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+let mysql = require('mysql')
+
+let connection = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'123456',
+  port:'3306',
+  database:'node'
+})
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
+});
+router.get('/product', function(req, res, next) {
+  res.render('product');
+});
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+router.post('/login', function(req, res) {
+  connection.query("select password from user where userName=? and password=?",[req.body.account,req.body.password],(err,data)=>{
+    if(err){
+      res.end("failure")
+    }else{if(data.length>0){res.redirect("/")}else{res.end("failure")}}
+  })
+})
+router.get('/register', function(req, res, next) {
+  res.render('register');
+})
+router.post("/register",(req,res)=>{
+  connection.query("insert into user(userName,telephonenumber,password)values(?,?,?)",[req.body.account,req.body.phone,req.body.password],(err,data)=>{
+    if(err){
+      res.end("failure")
+    }else{res.redirect("/")}
+  })
+})
+module.exports = router;
+
